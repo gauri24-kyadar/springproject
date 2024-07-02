ebaf0dad41ebdbe67386d0786f6ccc5db256bc98
Lenovo <Lenovo@DESKTOP-RGG94F2>
2024-07-02 15:14:22 +0530

    commit

---
+++ springbootmanytomany/src/main/java/com/symbiosis/SpringbootmanytomanyApplication.java
@@ -0,0 +1,13 @@
+package com.symbiosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootmanytomanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootmanytomanyApplication.class, args);
+	}
+
+}
